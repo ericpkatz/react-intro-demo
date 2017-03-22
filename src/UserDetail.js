3efd5799f05fc1908d868a529567190d4137d9d8
Eric P. Katz <ericpkatz@gmail.com>
2017-03-21 20:58:11 -0400

    commit

---
+++ src/UserDetail.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const UserDetail = ({ user, onDeselect })=>(
+  <div className='well' onClick={()=> onDeselect() }>
+    { user.name }
+  </div>
+);
+export default UserDetail;
